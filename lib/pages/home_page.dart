2994aec63e8abab758e043c3e0a915a01417d842
Souva <schea1979@hotmail.fr>
2020-10-31 21:30:31 +0100

    4eme commit fichiers pages, providers, tabs, widget et homepage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:portfoliosuva/config/assets.dart';
+import 'package:portfoliosuva/tabs/about_tab.dart';
+import 'package:portfoliosuva/tabs/blog_tab.dart';
+import 'package:portfoliosuva/tabs/projects_tab.dart';
+import 'package:portfoliosuva/widgets/theme_inherited_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage>{
+  int _selectedIndex = 0;
+  static List<Widget> tabWidgets = <Widget>[
+    AboutTab(),
+    BlogTab(),
+    ProjectsTab(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Center(
+        child: tabWidgets.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text('About'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chrome_reader_mode),
+            title: Text('Blog'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mobile_screen_share),
+            title: Text('Projects'),
+          )
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index)=> setState(() => _selectedIndex = index),
+        selectedItemColor: Theme.of(context).accentColor,
+      ),
+    );
+  }
+
+}
