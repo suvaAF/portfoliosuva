1c6273906b6945c74a06bfed588c918978fe2399
Souva <schea1979@hotmail.fr>
2020-10-31 22:32:15 +0100

    7ème commit fichiers api et blog

---
+++ lib/providers/api_provider.dart
@@ -0,0 +1,12 @@
+class ApiProvider{
+  Future<List<Blog>> getBlogs() async {
+    final response = await http.get(Constants.BLOG_API);
+    if(response.statusCode==200){
+      List blogs = json.decode(utf8.decode(response.bodyBytes))['response'];
+      final results= blogs.map((blog)=>Blog.fromJson(blog)).toList();
+      return results;
+    }else{
+      return null;
+    }
+  }
+}
